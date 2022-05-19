532f43f868dbe5d1a0319c1b6ea154e26fc6be1e
Mario Gómez <devrgomez@gmail.com>
2022-05-18 23:03:08 -0500

    second commit

--- nav.js
+++ nav.js
@@ -1,18 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const hamburger = document.querySelector(".maqui-nav-menu");
+const navLinks = document.querySelector(".maqui-nav-links");
+const links = document.querySelectorAll(".maqui-nav-links li");
 
-hamburger.addEventListener('click', () => {
+let deployLinks = function () {
     //Animate Links
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
-
-
         var line1 = document.querySelector(".line1");
         var line2 = document.querySelector(".line2");
         var line3 = document.querySelector(".line3");
-        if (hamburger.classList.contains("toggle")) {
+        if (hamburger.classList.contains("nav-toggle")) {
             line1.style.width = "35px";
             line2.style.width = "35px";
             line3.style.width = "15px";
@@ -22,8 +20,9 @@ hamburger.addEventListener('click', () => {
             line2.style.width = "35px";
             line3.style.width = "0px";
         }
+        link.addEventListener('click', deployLinks);
     });
-
     //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-});
+    hamburger.classList.toggle("nav-toggle");
+}
+hamburger.addEventListener('click', deployLinks);
